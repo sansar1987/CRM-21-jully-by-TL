e6a83588eabe4d21bcafa5e64153035cd59fd900
ok <priyapawar32415@gmail.com>
2024-07-21 14:48:03 +0530

    DemoProject created by TL

---
+++ src/main/java/PageLayer/LoginPage.java
@@ -0,0 +1,10 @@
+package PageLayer;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Login Page Created by TL ");
+	}
+
+}
---
+++ src/test/java/Features/TC0001CRMFunctionality.feature
@@ -0,0 +1,5 @@
+Feature: CRM functionality
+ Scenario: Validate Login Page Functionality
+    Given user is on login page
+    When user enter valid credentails
+    Then user click on login button
